ff19ba408e398e225fec2788802c933c915f7e36
Nurkyz <rahmanalievan181@gmail.com>
2021-05-22 19:29:57 +0600

    forms

--- mysite/client/views.py
+++ mysite/client/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render
 from client.models import Client
+from django.forms import ModelForm
 
+
+class ClientForm(ModelForm):
+    class Meta:
+        model = Client
+        fields = "__all__"
 # Create your views here.
 def client(request):
+    if request.method == "POST":   
+        form = ClientForm(request.POST)
+        form.save()
+    #     name = request.POST.get("client_name")
+    #     surname = request.POST.get("client_surname")
+    #     age = request.POST.get("client_age")
+    #     if name and surname and age:
+    #         Client.objects.create(name=name, surname=surname, age=age)
+
     clients = Client.objects.all()
-    
     return render(request,"client/client.html",{"clients":clients})
 
 def client_detail(request,id):
--- mysite/doctor/views.py
+++ mysite/doctor/views.py
@@ -1,12 +1,29 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
 from doctor.models import Doctor
+from django.forms import ModelForm
 
+class DoctorForm(ModelForm):
+    class Meta:
+        model = Doctor
+        fields = "__all__"
 # Create your views here.
 def doctor(request):
-    doctors = Doctor.objects.all()
-    return render(request,"doctor/doctor.html",{"doctors":doctors})
+    doctor_form= DoctorForm()
+    if request.method == "POST":   
+        doctor_form = DoctorForm(request.POST,request.FILES)
+        if doctor_form.is_valid():
+            doctor_form.save()
+        
+        # name = request.POST.get("doctor_name")
+        # surname = request.POST.get("doctor_surname")
+        # age = request.POST.get("doctor_age")
+        # if name and surname and age:
+        #     Doctor.objects.create(name=name, surname=surname, age=age)
 
+    doctors = Doctor.objects.all()
+    return render(request,"doctor/doctor.html",{"doctors":doctors,"doctor_form":doctor_form})
+    
 def doctor_detail(request,id):
     doctor = Doctor.objects.get(id=id)
     return render(request,"doctor/detail.html",{"doctor":doctor})
